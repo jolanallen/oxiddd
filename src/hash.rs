626bee8b19b4fa9efc05e521baa1578b7af64944
jolan <jolanallen34@gmail.com>
2026-02-25 14:29:22 +0100

    style: apply cargo fmt to fix CI failures

--- src/hash.rs
+++ src/hash.rs
@@ -1,4 +1,4 @@
-use sha2::{Sha256, Sha512, Digest};
+use sha2::{Digest, Sha256, Sha512};
 
 pub enum HashAlgo {
     Sha256,
@@ -36,18 +36,18 @@ impl ForensicHasher {
             HasherInstance::Sha256(std, custom) => {
                 std.update(data);
                 custom.update(data);
-            },
+            }
             HasherInstance::Sha512(std, custom) => {
                 std.update(data);
                 custom.update(data);
-            },
+            }
         }
     }
 
     pub fn finalize(self) -> (String, String) {
         let name_bytes = self.target_filename.as_bytes();
         let ts_bytes = self.ntp_timestamp.as_bytes();
-        
+
         match self.instance {
             HasherInstance::Sha256(std, mut custom) => {
                 let std_hash = hex::encode(std.finalize());
@@ -55,17 +55,17 @@ impl ForensicHasher {
                 custom.update(ts_bytes);
                 let custom_hash = hex::encode(custom.finalize());
                 (std_hash, custom_hash)
-            },
+            }
             HasherInstance::Sha512(std, mut custom) => {
                 let std_hash = hex::encode(std.finalize());
                 custom.update(name_bytes);
                 custom.update(ts_bytes);
                 let custom_hash = hex::encode(custom.finalize());
                 (std_hash, custom_hash)
-            },
+            }
         }
     }
-    
+
     pub fn extension(&self) -> &str {
         match self.algo {
             HashAlgo::Sha256 => "sha256",
@@ -82,16 +82,16 @@ mod tests {
     fn test_forensic_binding() {
         let data = b"hello forensic world";
         let ts = "2026-02-25T120000Z";
-        
+
         let mut h1 = ForensicHasher::new(HashAlgo::Sha256, "file1.dd".to_string(), ts.to_string());
         let mut h2 = ForensicHasher::new(HashAlgo::Sha256, "file2.dd".to_string(), ts.to_string());
-        
+
         h1.update(data);
         h2.update(data);
-        
+
         let (std1, cust1) = h1.finalize();
         let (std2, cust2) = h2.finalize();
-        
+
         assert_eq!(std1, std2);
         assert_ne!(cust1, cust2);
     }
